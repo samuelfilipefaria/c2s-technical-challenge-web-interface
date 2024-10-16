2888e881a752357d5782610311e7884b4886edd1
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-10-16 20:04:58 -0300

    feat(app): creating register form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,3 +1,21 @@
+<script setup>
+import RegisterForm from "@/components/RegisterForm.vue"
+</script>
+
 <template>
-  <h1>Register</h1>
+  <v-container class="text-center mt-5">
+    <v-row>
+      <v-col cols="12">
+        <h1>Register</h1>
+      </v-col>
+
+      <v-col cols="12">
+        <RegisterForm/>
+      </v-col>
+
+      <v-col cols="12">
+        Already have an account? <RouterLink to="/login">Login <v-icon icon="mdi-open-in-new"></v-icon></RouterLink>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
