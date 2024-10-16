b61428c9dbc9cf7758fdc1b98c2ab66d86204791
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-10-16 19:52:36 -0300

    feat(app): Creating login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,54 @@
+<template>
+<v-sheet class="mx-auto" width="300">
+  <v-form fast-fail @submit.prevent>
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      type="text"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      :rules="passwordRules"
+      label="Password"
+      type="password"
+    ></v-text-field>
+
+    <v-btn class="mt-2 bg-light-blue" type="submit" block>Login</v-btn>
+  </v-form>
+</v-sheet>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      email: '',
+      emailRules: [
+        value => {
+          if (value) return true
+
+          return 'E-mail is required'
+        },
+        value => {
+          if (/.+@.+\..+/.test(value)) return true
+
+          return 'E-mail must be valid'
+        },
+      ],
+      password: '123',
+      passwordRules: [
+        value => {
+          if (value) return true
+
+          return 'Password is required.'
+        },
+        value => {
+          if (value.length >= 8) return true
+
+          return 'Min 8 characters'
+        },
+      ],
+    }),
+  }
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,21 @@
+<script setup>
+import LoginForm from "@/components/LoginForm.vue"
+</script>
+
 <template>
-  <h1>Login</h1>
+  <v-container class="text-center mt-5">
+    <v-row>
+      <v-col cols="12">
+        <h1>Login</h1>
+      </v-col>
+
+      <v-col cols="12">
+        <LoginForm/>
+      </v-col>
+
+      <v-col cols="12">
+        Don't have an account yet? <RouterLink to="/register">Register</RouterLink>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
